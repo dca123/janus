20d259bf8108b7e4ee1e6066590815991028f782
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2023-10-27 00:29:11 +0800

    forecast table

--- app/lib/hubspot.ts
+++ app/lib/hubspot.ts
@@ -1,10 +1,31 @@
 import { Client } from '@hubspot/api-client';
-import { getTime } from 'date-fns';
+import { addMonths, getTime } from 'date-fns';
 import { z } from 'zod';
 
-const hubspotClient = new Client({ accessToken: process.env.HUBSPOT_API_KEY });
+export const DEAL_STAGES = {
+  '26411178': 'Lead Opportunity',
+  appointmentscheduled: 'Early Discussion',
+  qualifiedtobuy: 'Qualified Opportunity',
+  decisionmakerboughtin: 'Decision maker bought-In',
+  contractsent: 'Contract sent',
+  closedwon: 'Closed won',
+  closedlost: 'Closed lost',
+  '29291955': 'Staging',
+  '29281205': 'Delivery - Project',
+  '29281206': 'Delivery - Managed Service',
+  '29281207': 'Final Invoice',
+};
 
-function currentFiscalYear() {
+export const DEAL_STAGE_PROBABILITY = {
+  '26411178': 0.05,
+  appointmentscheduled: 0.2,
+  qualifiedtobuy: 0.4,
+  decisionmakerboughtin: 0.8,
+  contractsent: 0.9,
+  closedwon: 1,
+};
+
+export function currentFiscalYear() {
   const today = new Date();
   const fiscalYearStart = new Date(today.getFullYear(), 6, 1); // July 1st
   if (today < fiscalYearStart) {
@@ -24,31 +45,55 @@ export class HubSpot {
       accessToken: process.env.HUBSPOT_API_KEY,
     });
   }
-  async dealStages() {
-    const response = await this.client.crm.properties.coreApi.getByName(
-      'deals',
-      'dealstage',
-      false,
-    );
-    console.log(response);
-  }
 
   async deals() {
     const Schema = z.array(
-      z.object({
-        id: z.string(),
-        properties: z.object({
-          amount: z.string().nullable(),
-          createdate: z.string(),
-          dealname: z.string(),
-          dealstage: z.string(),
-          hs_lastmodifieddate: z.string(),
-          hs_object_id: z.string(),
+      z
+        .object({
+          id: z.string(),
+          properties: z.object({
+            amount: z.string().nullable(),
+            createdate: z.string(),
+            closedate: z.string(),
+            dealname: z.string(),
+            dealstage: z.string(),
+            hs_lastmodifieddate: z.string(),
+            hs_object_id: z.string(),
+          }),
+          createdAt: z.date(),
+          updatedAt: z.date(),
+          archived: z.boolean(),
+        })
+        .transform((data) => {
+          const probability =
+            DEAL_STAGE_PROBABILITY[
+              data.properties.dealstage as keyof typeof DEAL_STAGE_PROBABILITY
+            ];
+          const adjustedAmount = Number(data.properties.amount) * probability;
+          const closeDate = new Date(data.properties.closedate);
+          const forecast = {
+            month1: {
+              date: addMonths(closeDate, 1),
+              amount: adjustedAmount * 0.4,
+            },
+            month2: {
+              date: addMonths(closeDate, 2),
+              amount: adjustedAmount * 0.3,
+            },
+            month3: {
+              date: addMonths(closeDate, 3),
+              amount: adjustedAmount * 0.3,
+            },
+          };
+          return {
+            ...data,
+            properties: {
+              ...data.properties,
+              probability,
+              forecast,
+            },
+          };
         }),
-        createdAt: z.date(),
-        updatedAt: z.date(),
-        archived: z.boolean(),
-      }),
     );
     const response = await this.client.crm.deals.searchApi.doSearch({
       filterGroups: [
@@ -70,7 +115,7 @@ export class HubSpot {
       ],
       after: 0,
       limit: 100,
-      properties: ['dealname', 'dealstage', 'amount'],
+      properties: ['dealname', 'dealstage', 'amount', 'closedate'],
       sorts: ['dealname'],
     });
     const deals = Schema.parse(response.results);
@@ -81,7 +126,7 @@ export class HubSpot {
 // async function main() {
 //   const hubspot = new HubSpot();
 //   const deals = await hubspot.dealStages();
-//   console.log(deals);
+//   // console.log(deals);
 // }
 
 // main();
--- app/routes/forecast.tsx
+++ app/routes/forecast.tsx
@@ -9,7 +9,8 @@ import {
   TableHeader,
   TableRow,
 } from '~/components/ui/table';
-import { HubSpot } from '~/lib/hubspot';
+import { addMonths, format, isSameMonth } from 'date-fns';
+import { DEAL_STAGES, HubSpot, currentFiscalYear } from '~/lib/hubspot';
 
 export async function loader() {
   const hubspot = new HubSpot();
@@ -22,8 +23,20 @@ const AuDollar = new Intl.NumberFormat('en-AU', {
   currency: 'AUD',
 });
 
+function monthsInFiscalYear() {
+  const { start } = currentFiscalYear();
+  console.log(start);
+  const months = [];
+  for (let index = 0; index < 12; index++) {
+    const month = addMonths(start, index);
+    months.push(month);
+  }
+  return months;
+}
+
 export default function Forecast() {
   const { deals } = useLoaderData<typeof loader>();
+  const months = monthsInFiscalYear();
   return (
     <div className="flex flex-col space-y-2">
       <div className="flex flex-row space-x-2 items-center p-6 justify-center mx-auto">
@@ -34,9 +47,15 @@ export default function Forecast() {
         <TableCaption>Deals for this fiscal year</TableCaption>
         <TableHeader>
           <TableRow>
-            <TableHead>Name</TableHead>
+            <TableHead className="w-[100px]">Name</TableHead>
             <TableHead>Stage</TableHead>
             <TableHead className="text-right">Amount</TableHead>
+            <TableHead>Close Date</TableHead>
+            {months.map((month) => (
+              <TableHead key={month.getMonth()}>
+                {format(month, 'MMM / yy')}
+              </TableHead>
+            ))}
           </TableRow>
         </TableHeader>
         <TableBody>
@@ -45,14 +64,79 @@ export default function Forecast() {
               <TableCell className="font-medium">
                 {deal.properties.dealname}
               </TableCell>
-              <TableCell>{deal.properties.dealstage}</TableCell>
+              <TableCell>
+                {
+                  DEAL_STAGES[
+                    deal.properties.dealstage as keyof typeof DEAL_STAGES
+                  ]
+                }
+              </TableCell>
               <TableCell className="text-right">
                 {AuDollar.format(parseInt(deal.properties.amount ?? '0'))}
               </TableCell>
+              <TableCell>
+                {format(new Date(deal.properties.closedate), 'd MMM yyyy')}
+              </TableCell>
+              {months.map((month) => (
+                <ForecastCell
+                  key={`${month.getMonth()}-${deal.id}`}
+                  forcast={deal.properties.forecast}
+                  calendarDate={month.toISOString()}
+                />
+              ))}
             </TableRow>
           ))}
         </TableBody>
       </Table>
     </div>
   );
+}
+
+type ForecastCellProps = {
+  forcast: {
+    month1: {
+      date: string;
+      amount: number;
+    };
+    month2: {
+      date: string;
+      amount: number;
+    };
+    month3: {
+      date: string;
+      amount: number;
+    };
+  };
+  calendarDate: string;
+};
+function ForecastCell(props: ForecastCellProps) {
+  const calendarDate = new Date(props.calendarDate);
+  const month1Date = new Date(props.forcast.month1.date);
+  const month2Date = new Date(props.forcast.month2.date);
+  const month3Date = new Date(props.forcast.month3.date);
+
+  if (isSameMonth(calendarDate, month1Date)) {
+    return (
+      <TableCell className="text-right">
+        {AuDollar.format(props.forcast.month1.amount)}
+      </TableCell>
+    );
+  }
+  if (isSameMonth(calendarDate, month2Date)) {
+    return (
+      <TableCell className="text-right">
+        {AuDollar.format(props.forcast.month2.amount)}
+      </TableCell>
+    );
+  }
+
+  if (isSameMonth(calendarDate, month3Date)) {
+    return (
+      <TableCell className="text-right">
+        {AuDollar.format(props.forcast.month3.amount)}
+      </TableCell>
+    );
+  }
+
+  return <TableCell className="text-right">-</TableCell>;
 }
